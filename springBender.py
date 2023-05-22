43a9aa2ef27f5b4d4dcc0b32c2a0faa5dc9bf296
Unsweeticetea <iamzevle@gmail.com>
2023-05-22 00:35:30 -0400

    It works now
    Gcode generation

--- springBender.py
+++ springBender.py
@@ -1,7 +1,8 @@
 '''Runs a DIY desktop continuous spring bender'''
-import sys
+from pathlib import Path
+import sys, os
 from sympy.physics.units import *
-from sympy import pi, sqrt, N, Eq, symbols, solve, nsolve, solve_poly_system
+from sympy import pi, sqrt, N, Eq, symbols, solve
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QPalette, QColor
@@ -67,6 +68,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         if self.sb_d.value() != 0:
             d= self.sb_d.value()
             d*= inch if self.cb_d.currentIndex()==0 else mm
+            print(d)
         else:
             designZeros+=1
             missingItem=d
@@ -184,180 +186,73 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         #print(solve(eqBeta, eqBeta.lhs)[0])
         print(answer)
         #print(convert_to(answer,sibase))
+        p=.002*meter
+        deadlength=str(70)
+        livelength=str(round((N(pi)*D*5).as_coeff_Mul()[0]*1000,1))
+        xpos=str(round(23.4381/((OD.as_coeff_Mul()[0]*1000)**.14455),2))
+        pitch=str(round(p.as_coeff_Mul()[0]*1000,2)).rstrip("0").rstrip(".")
+        print(pitch)
+        file = str(os.path.join(Path.home(), "Downloads"))+"\spring.gcode"
+        with open(file, "w") as f:
+            code=\
+'''G21
+G90
+M83
 
-        '''print('ssy:',ssy, "\t\t\t\t\texpected:", convert_to(130454.9381*psi,sibase),\
-            '\nalpha:',alpha, "\t\t\t\t\texpected:", convert_to(108712.4484*psi,sibase),\
-            '\nbeta:', beta, "\t\t\t\t\texpected:", convert_to(9151.409228*psi,sibase),\
-            '\nC:', C, "\t\t\t\t\texpected:", convert_to(10.5334876,sibase),\
-            '\nD:', D, "\t\t\t\t\texpected:", convert_to(0.842679008*inch,sibase),\
-            '\nkb:', kb, "\t\t\t\t\texpected:", convert_to(1.127766299,sibase),\
-            '\ntau:', tau, "\t\t\t\t\texpected:", convert_to(108712.4484*psi,sibase),\
-            '\nOD:', OD, "\t\t\t\t\texpected:", convert_to(0.922679008*inch,sibase),\
-            '\nID:', ID, "\t\t\t\t\texpected:", convert_to(0.762679008*inch,sibase),\
-            '\nna:', na, "\t\t\t\t\texpected:", convert_to(10.05359304,sibase),\
-            '\nnt:', nt, "\t\t\t\t\texpected:", convert_to(12.05359304,sibase),\
-            '\np:', p, "\t\t\t\t\texpected:", convert_to(0.299395449*inch,sibase),\
-            '\nLs:', Lcs, "\t\t\t\t\texpected:", convert_to(0.964287444*inch,sibase),\
-            '\nLcr:', Lcr, "\t\t\t\t\texpected:", convert_to(4.432491582*inch,sibase))'''
+M117 Homing Y
+G28 Y
+M18 X
+M0 Move X home
+M17 X
+G92 E0 X0
 
-    '''def equations2(self):
-        sibase = UnitSystem.get_unit_system("SI")._base_units
-        designZeros = 0
-        materialZeros = 0
-        missingItem = None
-
-        F, Y, L, E, G, A, m, ns, xi, d, OD = symbols('F Y L E G A m ns xi d OD')
-        ssy, alpha, beta, C, D, kb, tau, ID, na, nt, p, Lcs, Lcr = symbols('ssy alpha beta C D kb tau ID na nt p Lcs Lcr')
-        
-        #test and assign design properties
-        if self.sb_F.value() != 0:
-            F= self.sb_F.value()
-            F*= lbf if self.cb_F.currentIndex()==0 else gf
-        else:
-            designZeros+=1
-            missingItem=F
-        if self.sb_Y.value() != 0:
-            Y= self.sb_Y.value()
-            Y*= inch if self.cb_Y.currentIndex()==0 else mm
-        else:
-            designZeros+=1
-            missingItem=Y
-        if self.sb_L.value() != 0:
-            L= self.sb_L.value()
-            L*= inch if self.cb_L.currentIndex()==0 else mm
-        else:
-            designZeros+=1
-            missingItem=L
-        if self.sb_d.value() != 0:
-            d= self.sb_d.value()
-            d*= inch if self.cb_d.currentIndex()==0 else mm
-        else:
-            designZeros+=1
-            missingItem=d
-        if self.sb_OD.value() != 0:
-            OD= self.sb_OD.value()
-            OD*= inch if self.cb_OD.currentIndex()==0 else mm
-        else:
-            designZeros+=1
-            missingItem=OD
-        
-        #test and assign material properties
-        if self.sb_E.value() != 0:
-            E= self.sb_E.value()
-            E*= mega*psi if self.cb_E.currentIndex()==0 else giga*Pa
-        else:
-            materialZeros+=1
-        if self.sb_G.value() != 0:
-            G= self.sb_G.value()
-            G*= mega*psi if self.cb_G.currentIndex()==0 else giga*Pa
-        else:
-            materialZeros+=1
-        if self.sb_m.value() != 0:
-            m= self.sb_m.value()
-        else:
-            materialZeros+=1
-        if self.sb_A.value() != 0:
-            A= self.sb_A.value()
-            A*= kilo*psi*inch**m if self.cb_A.currentIndex()==0 else mega*Pa*mm**m
-        else:
-            materialZeros+=1
-        if self.sb_ns.value() != 0:
-            ns= self.sb_ns.value()
-        else:
-            materialZeros+=1
-        if self.sb_Xi.value() != 0:
-            xi= self.sb_Xi.value()
-        else:
-            materialZeros+=1
-
-        #tests if proper number of zero values are present
-        if designZeros!=1 or materialZeros!=0:
-            #print("invalid inputs.\nDesign:",designZeros,"\nMaterial:",materialZeros)
-            return
-        else:
-            pass
-            #print("valid inputs.\nDesign:",designZeros,"\nMaterial:",materialZeros)
-
-        #eq=Eq(,convert_to(,sibase))
-        eqSsy=Eq(ssy,convert_to(.45*A/(d**m),sibase))
-        eqAlpha=Eq(alpha,convert_to(eqSsy.rhs/ns,sibase))
-        eqBeta=Eq(beta,convert_to((8*(1+xi)*F)/(N(pi)*(d**2)),sibase))
-        eqC=Eq(C,convert_to(((2*eqAlpha.rhs-eqBeta.rhs)/(4*eqBeta.rhs))+sqrt((((2*eqAlpha.rhs-eqBeta.rhs)/(4*eqBeta.rhs))**2)-(3*eqAlpha.rhs)/(4*eqBeta.rhs)),sibase))
-        eqD=Eq(D,convert_to(eqC.rhs*d,sibase))
-        eqkb=Eq(kb,convert_to((4*eqC.rhs+2)/(4*eqC.rhs-3),sibase))
-        eqtau=Eq(tau,convert_to(eqkb.rhs*8*F*(1+xi)*eqD.rhs/(N(pi)*d**3),sibase))
-        eqOD=Eq(OD,convert_to(eqD.rhs+d,sibase))
-        eqID=Eq(ID,convert_to(eqD.rhs-d,sibase))
-        eqna=Eq(na,convert_to((G*(d**4)*Y)/(8*(eqD.rhs**3)*F),sibase))
-        eqnt=Eq(nt,convert_to(eqna.rhs+2,sibase))
-        eqp=Eq(p,convert_to((L-3*d)/eqna.rhs,sibase))
-        eqLcs=Eq(Lcs,convert_to(eqnt.rhs*d,sibase))
-        eqLcr=Eq(Lcr,convert_to(2.63*eqD.rhs/.5,sibase))
-
-        print("solving for:",missingItem)
-        equations = [eqSsy, eqAlpha, eqBeta, eqC, eqD, eqkb, eqtau, eqOD, eqID, eqna, eqnt, eqp, eqLcs, eqLcr]
-        #equations = [eqOD, eqID, eqSsy, eqBeta, eqAlpha, eqC, eqD, eqkb, eqtau, eqna, eqnt, eqp, eqLcs, eqLcr]
-        #outputs = [F, Y, L, d, eqSsy.lhs, eqAlpha.lhs, eqBeta.lhs, eqC.lhs, eqD.lhs, eqkb.lhs, eqtau.lhs, eqOD.lhs, eqID.lhs, eqna.lhs, eqnt.lhs, eqp.lhs, eqLcs.lhs, eqLcr.lhs]
-        outputs = [eqSsy.lhs, eqAlpha.lhs, eqBeta.lhs, eqC.lhs, eqD.lhs, eqkb.lhs, eqtau.lhs, eqOD.lhs, eqID.lhs, eqna.lhs, eqnt.lhs, eqp.lhs, eqLcs.lhs, eqLcr.lhs]
-        #answer = nsolve(equations, 'OD', 1)
-        #answer = solve(equations, missingItem, minimal = True)
-        answer = solve_poly_system(equations, outputs)
-        #answer = solve(equations, missingItem)[0]
-        #print(solve(eqBeta, eqBeta.lhs)[0])
-        print(answer)
-        #print(convert_to(answer,sibase))
-         
-        #=solve(eq,eq.lhs)[0]
-        ssy=solve(eqSsy, eqSsy.lhs)[0]
-        alpha=solve(eqAlpha, eqAlpha.lhs)[0]
-        beta=solve(eqBeta, eqBeta.lhs)[0]
-        C=solve(eqC,eqC.lhs)[0]
-        D=solve(eqD,eqD.lhs)[0]
-        kb=solve(eqkb,eqkb.lhs)[0]
-        tau=solve(eqtau,eqtau.lhs)[0]
-        OD=solve(eqOD,eqOD.lhs)[0]
-        ID=solve(eqID,eqID.lhs)[0]
-        na=solve(eqna,eqna.lhs)[0]
-        nt=solve(eqnt,eqnt.lhs)[0]
-        p=solve(eqp,eqp.lhs)[0]
-        Lcs=solve(eqLcs,eqLcs.lhs)[0]
-        Lcr=solve(eqLcr,eqLcr.lhs)[0]
-        print('ssy:',ssy, "\t\t\t\t\texpected:", convert_to(130454.9381*psi,sibase),\
-            '\nalpha:',alpha, "\t\t\t\t\texpected:", convert_to(108712.4484*psi,sibase),\
-            '\nbeta:', beta, "\t\t\t\t\texpected:", convert_to(9151.409228*psi,sibase),\
-            '\nC:', C, "\t\t\t\t\texpected:", convert_to(10.5334876,sibase),\
-            '\nD:', D, "\t\t\t\t\texpected:", convert_to(0.842679008*inch,sibase),\
-            '\nkb:', kb, "\t\t\t\t\texpected:", convert_to(1.127766299,sibase),\
-            '\ntau:', tau, "\t\t\t\t\texpected:", convert_to(108712.4484*psi,sibase),\
-            '\nOD:', OD, "\t\t\t\t\texpected:", convert_to(0.922679008*inch,sibase),\
-            '\nID:', ID, "\t\t\t\t\texpected:", convert_to(0.762679008*inch,sibase),\
-            '\nna:', na, "\t\t\t\t\texpected:", convert_to(10.05359304,sibase),\
-            '\nnt:', nt, "\t\t\t\t\texpected:", convert_to(12.05359304,sibase),\
-            '\np:', p, "\t\t\t\t\texpected:", convert_to(0.299395449*inch,sibase),\
-            '\nLs:', Lcs, "\t\t\t\t\texpected:", convert_to(0.964287444*inch,sibase),\
-            '\nLcr:', Lcr, "\t\t\t\t\texpected:", convert_to(4.432491582*inch,sibase))'''
+M117 Priming wire
+G1 X9 Y17.1
+G91
+G1 E3.5 F350
+M117 Creating dead coils
+G90
+G1 X'''+xpos+'''
+G91
+G1 E'''+deadlength+''' F750
+;M0 Are coils on arm?
+G1 Y-'''+pitch+'''
+M117 Creating live coils
+G1 E'''+livelength+''' F750
+M117 Creating dead coils
+G1 Y'''+pitch+'''
+G1 E'''+deadlength+''' F750
+G90
+G1 X9
+G91
+G1 E40 F250
+M0 Cut spring then press dial
+G1 E-42 F1000
+G1 E2 F500
+'''
+            f.write(code)
 
 
     def test(self):
         
-        self.sb_F.setValue(20)
+        self.sb_F.setValue(1.025)
         self.cb_F.setCurrentIndex(0)
-        self.sb_Y.setValue(2)
-        self.cb_Y.setCurrentIndex(0)
-        self.sb_L.setValue(3.25)
-        self.cb_L.setCurrentIndex(0)
-        self.sb_d.setValue(.08)
+        self.sb_Y.setValue(2.6)
+        self.cb_Y.setCurrentIndex(1)
+        self.sb_L.setValue(22.6)
+        self.cb_L.setCurrentIndex(1)
+        self.sb_d.setValue(.023)
         self.cb_d.setCurrentIndex(0)
-        self.sb_OD.setValue(0.922679008)
+        self.sb_OD.setValue(0.45)
         self.cb_OD.setCurrentIndex(0)
         #determine OD
-        if False:
+        if True:
             self.sb_OD.setValue(0)
             self.cb_OD.setCurrentIndex(0)
         elif False:
             self.sb_d.setValue(0)
             self.cb_d.setCurrentIndex(0)
-        elif True:
+        elif False:
             self.sb_Y.setValue(0)
             self.cb_Y.setCurrentIndex(0)
         #determine D
